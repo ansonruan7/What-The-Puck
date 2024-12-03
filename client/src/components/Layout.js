68d1ac33500bfb69e666e8d264e680a3015e2940
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2024-12-02 20:48:23 -0500

    Login/Logout mutually exclusive

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,55 +1,56 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Layout.css';
 import { useUser } from './UserContext';
 
-function Layout ({children}) {
+function Layout({ children }) {
     const navigate = useNavigate();
     const navigateHome = () => navigate('/');
     const navigateCreateAccount = () => navigate('/CreateAccount');
-    const navigateLogin =() => navigate('/Login');
-    const navigateUserPortal =() => navigate('/AuthUser');
+    const navigateLogin = () => navigate('/Login');
+    const navigateUserPortal = () => navigate('/AuthUser');
 
-    // Temporary code
-    const navigateCoachDashboard = () => navigate("/CoachDashboard");
-    const navigateAdminDashboard = () => navigate("/AdminDashboard");
+    // Temporary code for navigation
+    const navigateCoachDashboard = () => navigate('/CoachDashboard');
+    const navigateAdminDashboard = () => navigate('/AdminDashboard');
 
-    const { logoutUser } = useUser();
+    const { user, logoutUser } = useUser();
 
-
-
-    const handleLogout = async() => {
+    const handleLogout = () => {
         logoutUser();
-          localStorage.removeItem('token');
-          navigate('/login');
-
-    }
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
 
     return (
         <div className="container">
             <h1>What The Puck?!</h1>
-            <div className='log-out'>
-                <button onClick={handleLogout}>Log Out</button>
-                </div>
+
             <div className="nav-links">
                 <a href="#" onClick={navigateHome}>Home</a>
                 <a href="#" onClick={navigateUserPortal}>User Portal</a>
-                <a href="#" onClick={navigateLogin}>Log in</a>
+                {user ? (
+                    <a href="#" onClick={handleLogout}>Logout</a>
+                ) : (
+                    <a href="#" onClick={navigateLogin}>Login</a>
+                )}
                 <a href="#" onClick={navigateCoachDashboard}>Coach Dashboard</a>
                 <a href="#" onClick={navigateAdminDashboard}>Admin Dashboard</a>
             </div>
-            <div className="cartButtonStyle">
+
+            {!user && (
+                <div className="cartButtonStyle">
                     <a href="#" className="buttonTextStyle" onClick={navigateCreateAccount}>
                         Create Account
                     </a>
                 </div>
-                <main className="main-content">
+            )}
+
+            <main className="main-content">
                 {children}
             </main>
-            </div>
-
-        
-    )
+        </div>
+    );
 }
 
 export default Layout;
